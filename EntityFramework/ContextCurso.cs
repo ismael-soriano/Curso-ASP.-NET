dfb487b71789d6dcd97cc69473043078a8dded2f
ismael-soriano <luzaoban@gmail.com>
2016-04-13 17:29:02 +0200

    -

---
+++ EntityFramework/ContextCurso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    public class ContextCurso : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*using (var ctx = new ContextCurso())
+            {
+                
+                var customer = new Customer()
+                {
+                    Name = "Pedro",
+                    Phone = "616647015"
+                };
+                ctx.Customers.Add(customer);
+                ctx.SaveChanges();
+            }*/
+
+            using (var ctx = new ContextCurso())
+            {
+                var x = ctx.Database.Connection.ConnectionString;
+                var customers = ctx.Customers;
+                foreach (var item in customers)
+                {
+                    Console.WriteLine(item.Name);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
